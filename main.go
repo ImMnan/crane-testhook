14224e22cdfce80a18b7e553ec2565ac29a3e9a2
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-05-16 10:23:59 +0530

    organised execution through pkg

--- main.go
+++ main.go
@@ -1,145 +1,20 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
 
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"github.com/immnan/crane-testhook/pkg"
 )
 
 func main() {
-
-	statusErr := StatusError{}
+	statusErr := pkg.StatusError{}
 	fmt.Println("Starting the requirements check...")
-	cs := &clientSet{}
-	cs.getClientSet()
-	//config, err := rest.InClusterConfig()
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-	//clientset, err := kubernetes.NewForConfig(config)
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-	//	wg.add(1)
-	//listNodesDetails(clientset)
-	statusErr.networkCheckBlaze()
-	statusErr.networkCheckImageRegistry()
-	statusErr.networkCheckThirdParty()
-	statusErr.listNodesDetails(clientSet{})
 
-	ingressType := os.Getenv("KUBERNETES_WEB_EXPOSE_TYPE")
-	if ingressType != "" {
-		statusErr.checkIngress(clientSet{})
-	}
+	pkg.Execute()
 
-	err := consolidation(&statusErr)
+	err := pkg.Consolidation(&statusErr)
 	if err != nil {
-		//fmt.Println("\n")
 		panic(err)
 	}
 	fmt.Println("\nAll checks passed successfully")
-}
-
-//namespace := "default" // replace with your namespace
-
-type StatusError struct {
-	NodeStatus                 error
-	NodeResourceStatus         []map[string]error
-	BlazeNetworkStatus         []map[string]error
-	ImageRegistryNetworkStatus error
-	ThirdPartyNetworkStatus    []map[string]error
-	IngressStatus              error
-}
-
-type clientSet struct {
-	clientset *kubernetes.Clientset
-}
-
-func (cs *clientSet) getClientSet() {
-	// Create a new Kubernetes client
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	//return clientset
-	cs.clientset = clientset
-}
-
-func consolidation(statusErr *StatusError) error {
-	// Check NodeStatus
-	if statusErr.NodeStatus != nil {
-		return errors.New("requirements check failed")
-	}
-
-	// Check NodeResourceStatus
-	for _, resourceStatus := range statusErr.NodeResourceStatus {
-		for _, err := range resourceStatus {
-			if err != nil {
-				return errors.New("requirements check failed")
-			}
-		}
-	}
-
-	// Check BlazeNetworkStatus
-	for _, blazeStatus := range statusErr.BlazeNetworkStatus {
-		for _, err := range blazeStatus {
-			if err != nil {
-				return errors.New("requirements check failed")
-			}
-		}
-	}
-
-	// Check ImageRegistryNetworkStatus
-	if statusErr.ImageRegistryNetworkStatus != nil {
-		return errors.New("requirements check failed")
-	}
-
-	// Check ThirdPartyNetworkStatus
-	for _, thirdPartyStatus := range statusErr.ThirdPartyNetworkStatus {
-		for _, err := range thirdPartyStatus {
-			if err != nil {
-				return errors.New("requirements check failed")
-			}
-		}
-	}
-	// Check IngressAvailability
-	if statusErr.IngressStatus != nil {
-		return errors.New("requirements check failed")
-	}
-	return nil
-}
-
-// Helper functions:
-// contains checks if a string is present in a slice.
-func contains(slice []string, item string) bool {
-	for _, s := range slice {
-		if s == item {
-			return true
-		}
-	}
-	return false
-}
-
-// containsAll checks if all elements of `subset` are present in `set`.
-func containsAll(set []string, subset []string) bool {
-	for _, sub := range subset {
-		found := false
-		for _, s := range set {
-			if s == sub {
-				found = true
-				break
-			}
-		}
-		if !found {
-			return false
-		}
-	}
-	return true
 }
---
+++ pkg/execute.go
@@ -0,0 +1,129 @@
+package pkg
+
+import (
+	"errors"
+	"os"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+//namespace := "default" // replace with your namespace
+
+type StatusError struct {
+	NodeStatus                 error
+	NodeResourceStatus         []map[string]error
+	BlazeNetworkStatus         []map[string]error
+	ImageRegistryNetworkStatus error
+	ThirdPartyNetworkStatus    []map[string]error
+	IngressStatus              error
+	RBAC                       error
+}
+
+type ClientSet struct {
+	clientset *kubernetes.Clientset
+}
+
+func (cs *ClientSet) getClientSet() {
+	// Create a new Kubernetes client
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	//return clientset
+	cs.clientset = clientset
+}
+
+func Execute() {
+	// This is a test function to check if the package is working
+	cs := ClientSet{}
+	cs.getClientSet()
+	statusError := StatusError{}
+
+	statusError.networkCheckBlaze()
+	statusError.networkCheckImageRegistry()
+	statusError.networkCheckThirdParty()
+	statusError.listNodesDetails(&cs)
+	statusError.rbacDefault(&cs)
+
+	svEnabled := os.Getenv("SV_ENABLE")
+	if svEnabled == "true" {
+		statusError.checkIngress(&cs)
+	}
+}
+
+func Consolidation(statusErr *StatusError) error {
+	// Check NodeStatus
+	if statusErr.NodeStatus != nil {
+		return errors.New("requirements check failed")
+	}
+
+	// Check NodeResourceStatus
+	for _, resourceStatus := range statusErr.NodeResourceStatus {
+		for _, err := range resourceStatus {
+			if err != nil {
+				return errors.New("requirements check failed")
+			}
+		}
+	}
+
+	// Check BlazeNetworkStatus
+	for _, blazeStatus := range statusErr.BlazeNetworkStatus {
+		for _, err := range blazeStatus {
+			if err != nil {
+				return errors.New("requirements check failed")
+			}
+		}
+	}
+
+	// Check ImageRegistryNetworkStatus
+	if statusErr.ImageRegistryNetworkStatus != nil {
+		return errors.New("requirements check failed")
+	}
+
+	// Check ThirdPartyNetworkStatus
+	for _, thirdPartyStatus := range statusErr.ThirdPartyNetworkStatus {
+		for _, err := range thirdPartyStatus {
+			if err != nil {
+				return errors.New("requirements check failed")
+			}
+		}
+	}
+	// Check IngressAvailability
+	if statusErr.IngressStatus != nil {
+		return errors.New("requirements check failed")
+	}
+	return nil
+}
+
+// Helper functions:
+// contains checks if a string is present in a slice.
+func contains(slice []string, item string) bool {
+	for _, s := range slice {
+		if s == item {
+			return true
+		}
+	}
+	return false
+}
+
+// containsAll checks if all elements of `subset` are present in `set`.
+func containsAll(set []string, subset []string) bool {
+	for _, sub := range subset {
+		found := false
+		for _, s := range set {
+			if s == sub {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}

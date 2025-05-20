dc5336a070ac994819a8e2ebe1717ab7ca7a443f
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-05-20 19:26:50 +0530

    build

--- containerImage/build.sh
+++ containerImage/build.sh
@@ -1,3 +1,6 @@
+#!bin/bash
+
+go env -w GOOS=linux GOARCH=amd64 && go build -o cranetest /Users/mpatel/Documents/GitHub/crane-testhook
 docker build -t cranetest .
 docker tag cranetest immnan/cranetest:0.1.0
 docker push immnan/cranetest:0.1.0

b5f72e9f2a8b2d3a56cd85006dd9d0f7625bb552
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-05-22 16:25:41 +0530

    build and dockerfile with ENV and other items

--- containerImage/build.sh
+++ containerImage/build.sh
@@ -1,7 +1,19 @@
-#!bin/bash
+#!/bin/bash
+set -e
 
-go env -w GOOS=linux GOARCH=amd64 && go build -o cranetest /Users/mpatel/Documents/GitHub/crane-testhook
+echo "Setting Go environment variables and building binary..."
+go env -w GOOS=linux GOARCH=amd64
+go build -o cranetest ../.
+
+echo "Building Docker image..."
 docker build -t cranetest .
-docker tag cranetest immnan/cranetest:0.1.0
-docker push immnan/cranetest:0.1.0
+
+echo "Cleaning up local binary..."
+rm cranetest
+
+echo "Tagging and pushing Docker image..."
+docker tag cranetest immnan/cranetest:0.1.22
+docker push immnan/cranetest:0.1.22
+
+echo "Listing Docker images..."
 docker images

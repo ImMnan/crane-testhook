f35d4cb5c9688450817ea89bb2789ffe6b5b3d02
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-05-22 16:25:25 +0530

    to meet hook requirements

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/immnan/crane-testhook/pkg"
@@ -13,7 +14,11 @@ func main() {
 
 	err := pkg.Consolidation(statusErr)
 	if err != nil {
+		fmt.Printf("\n\n[%s]", time.Now().Format("2006-01-02 15:04:05"))
 		panic(err)
+		os.Exit(1)
+
 	}
-	fmt.Printf("\n\n[%s] All checks passed successfully", time.Now())
+	fmt.Printf("\n\n[%s] All checks passed successfully, Private Location ready to accept Blazemeter Deployments\n", time.Now().Format("2006-01-02 15:04:05"))
+	os.Exit(0)
 }

c500c0af4cf094927d54b2082bf77c1bee057e37
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-05-26 10:28:01 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,11 +14,10 @@ func main() {
 
 	err := pkg.Consolidation(statusErr)
 	if err != nil {
-		fmt.Printf("\n\n[%s]", time.Now().Format("2006-01-02 15:04:05"))
-		panic(err)
+		fmt.Printf("\n\n[%s][error] K8s setup does not meet the requirement to run Blazemeter resources. \n%v\n", time.Now().Format("2006-01-02 15:04:05"), err)
+		os.Stdout.Sync() // flush output
 		os.Exit(1)
-
 	}
-	fmt.Printf("\n\n[%s] All checks passed successfully, Private Location ready to accept Blazemeter Deployments\n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Printf("\n\n[%s][DONE] All checks passed successfully, Private Location ready to accept Blazemeter Deployments\n", time.Now().Format("2006-01-02 15:04:05"))
 	os.Exit(0)
 }

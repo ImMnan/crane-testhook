e234cb89ab25dc73a938f5bb5f72bb918a163af5
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-06-05 14:58:32 +0530

    Update build.sh

--- containerImage/build.sh
+++ containerImage/build.sh
@@ -12,8 +12,11 @@ echo "Cleaning up local binary..."
 rm cranetest
 
 echo "Tagging and pushing Docker image..."
-docker tag cranetest immnan/cranetest:0.1.22
-docker push immnan/cranetest:0.1.22
+
+docker tag cranetest immnan/cranetest:0.2.10
+docker tag cranetest immnan/cranetest:latest
+docker push immnan/cranetest:0.2.10
+docker push immnan/cranetest:latest
 
 echo "Listing Docker images..."
 docker images
